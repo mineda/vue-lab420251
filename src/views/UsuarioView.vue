0c3a17c7aa5e34bdb6911f4fed0c3088e62a006e
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-03-11 21:16:10 -0300

    Página de usuário

---
+++ src/views/UsuarioView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>{{ nome }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const nome = ref<string>()
+
+nome.value = 'teste'
+</script>
